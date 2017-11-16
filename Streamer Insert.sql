f62ddda1b1136ed0faa53993bdd5bf779eaa6f7f
Wheeping <33730671+Wheeping@users.noreply.github.com>
2017-11-16 14:27:08 -0600

    Added Streamer Insert
    This is just a rough draft for an insert into the streamer table. It only requires the ChannelName. You can enter Pc Specs and Donations and so forth, but are Null by default

---
+++ Streamer Insert.sql
@@ -0,0 +1,23 @@
+Insert into streamer (ChannelName)
+Values
+("Blah"),
+("Bleh"),
+("Grg"),
+("Ilon"),
+("Nief"),
+("Peli"),
+("Nyme"),
+("Queo"),
+("Buej"),
+("Mnop"),
+("Zpol"),
+("Ypoe"),
+("Rety"),
+("Buppy"),
+("Rily"),
+("Loppy"),
+("Wuet"),
+("Leoh"),
+("Vino"),
+("Seco");
+Select * From streamer;
