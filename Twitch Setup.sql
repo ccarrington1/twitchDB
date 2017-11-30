424441b0f952e62d14e7963977c99188ddce5413
Wheeping <33730671+Wheeping@users.noreply.github.com>
2017-11-29 23:03:15 -0600

    Combined Setup with the Inserts

--- Twitch Setup.sql
+++ Twitch Setup.sql
@@ -181,6 +181,74 @@ CREATE TABLE IF NOT EXISTS `Twitch`.`GameStreaming` (
 
 ENGINE = InnoDB;
 
+-- ----------------------------------------------------
+-- Update and Add info throughout
+-- ----------------------------------------------------
+
+ALTER TABLE streamer ADD viewerID INT(45) AFTER streamerID;
+UPDATE streamer SET viewerID = streamerID;
+Set sql_safe_updates = 0;
+Insert into streamer (ChannelName)
+Values
+("Blah"),
+("Bleh"),
+("Grg"),
+("Ilon"),
+("Nief"),
+("Peli"),
+("Nyme"),
+("Queo"),
+("Buej"),
+("Mnop"),
+("Zpol"),
+("Ypoe"),
+("Rety"),
+("Buppy"),
+("Rily"),
+("Loppy"),
+("Wuet"),
+("Leoh"),
+("Vino"),
+("Seco");
+
+UPDATE streamer
+SET totalViews = FLOOR(RAND(10)*2000),
+    totalFollowers =  FLOOR(RAND(10)*600),
+    subscriptionButton = FLOOR(RAND() * 2) + 1,
+    Mature = FLOOR(RAND() * 2) + 1,
+    subIcon = FLOOR(RAND() * 2) + 1,
+    Donations = FLOOR(RAND(10)*1500);
+
+Insert into viewer (UserName)
+Values
+("Hello"),
+("Bob"),
+("Heaven"),
+("Seth"),
+("John"),
+("Charles"),
+("Jordan"),
+("Stacy"),
+("Amanda"),
+("Thor"),
+("Wonderwoman"),
+("Steve"),
+("Gregg"),
+("Amaz"),
+("Kripparian"),
+("Riggins"),
+("TimtheTatman"),
+("iijeriichoii"),
+("OneForAll_Gaming"),
+("Seraph"),
+("Donald_Trump");
+
+UPDATE viewer
+SET Doners = FLOOR(RAND() * 2) + 1,
+    Moderator =  FLOOR(RAND() * 2) + 1,
+    JoinDate = DATE_ADD('2011-06-05', INTERVAL  2356*rand() DAY);
+
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
