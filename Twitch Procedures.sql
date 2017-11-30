b2506c962af9e580e9cf4964c71fa3098cf0b246
Wheeping <33730671+Wheeping@users.noreply.github.com>
2017-11-30 13:55:33 -0600

    Messed up

--- Twitch Procedures.sql
+++ Twitch Procedures.sql
@@ -24,7 +24,6 @@ SET i = 0;
 WHILE i < 100 DO
 Set sql_safe_updates = 0;
 INSERT INTO moderator (streamerID, viewerID) VALUES (FLOOR(RAND()*20) +1, FLOOR(RAND()*20) +1);
-Call BirthdayReward((Select JoinDate FROM viewer WHERE viewerID = i));
 SET i = i + 1;
 END WHILE;
 END$$
